7444f852bf2c6f4f191645442cd20693a8229aac
darcyStephens <darcystephens40@gmail.com>
2023-10-12 21:50:17 +1030

    testing is complete

--- ItemTest.cpp
+++ ItemTest.cpp
@@ -7,12 +7,41 @@
 Logger logger;
 
 int main() {
-    std::cout<< "test item" << std::endl;
+    
+    std::cout<< "Test 26 test item" << std::endl;
     Item chestplate("plate", 0, 0, 4, 0);
     int armourBonus = chestplate.getArmourBonus(); // Get the armour bonus
 
     // Print the armour bonus to the console
     std::cout << "Armour Bonus: " << armourBonus << std::endl;
+
+    std::cout<< "Test 26 test gethealthbonus()" << std::endl;
+    chestplate.getHealthBonus();
+    if (chestplate.getHealthBonus() != 0)
+    {
+        return 1;
+    }
+
+    std::cout<< "Test 27 test getDamageBonus()" << std::endl;
+    chestplate.getDamageBonus();
+    if (chestplate.getDamageBonus() != 0)
+    {
+        return 1;
+    }
+
+    std::cout<< "Test 28 test getArmourBonus()" << std::endl;
+    chestplate.getArmourBonus();
+    if (chestplate.getArmourBonus() != 4)
+    {
+        return 1;
+    }
+
+     std::cout<< "Test 29 test getSpecialAttribute()" << std::endl;
+    chestplate.getSpecialAttribute();
+    if (chestplate.getSpecialAttribute() != 0)
+    {
+        return 1;
+    }
 
     return 0;
 }
--- PlayerTest.cpp
+++ PlayerTest.cpp
@@ -4,16 +4,139 @@
 #include "Player.h"
 #include "Mob.h"
 #include "Logger.h"
+#include "Item.h"
+#include "Boss.h"
 
 Logger logger;
 
 int main()
 {
-    std::cout<< "Test Player" << std::endl;
-    Player Steve("Steve", 10, 15, 20, 0);
+    Boss steve("steve", 50, 50, 50, "NOOOOOO!!");
+    Player greg("greg", 10, 15, 20, 0);
 
-    // Print the armour bonus to the console
-    std::cout << Steve.getBaseArmour() << std::endl;
+    // Print the armour to the console
+    std::cout<< "Test 14 testing getbaseamrour() \n";
+    std::cout << greg.getBaseArmour() << std::endl;
+    if (greg.getBaseArmour() != 20)
+    {
+        return 1;
+    }
 
-    return 0;
+    std::cout<< "Test 15 testing getBaseHealth() \n";
+    std::cout << greg.getBaseHealth() << std::endl;
+    if (greg.getBaseHealth() != 10)
+    {
+        return 1;
+    }
+    
+    std::cout<< "Test 16 testing getBaseDamage() \n";
+    std::cout << greg.getBaseDamage() << std::endl;
+    if (greg.getBaseDamage() != 15)
+    {
+        return 1;
+    }
+
+    std::cout<< "Test 17 testing getCurrentHealth() \n";
+    std::cout << greg.getCurrentHealth() << std::endl;
+    if (greg.getCurrentHealth() != 10)
+    {
+        return 1;
+    }
+
+    std::cout<< "Test 18 testing takeDamage() \n";
+    greg.takeDamage(5);
+    std::cout << greg.getCurrentArmour() << std::endl;
+    if (greg.getCurrentArmour() != 15)
+    {
+        return 1;
+    }
+
+    std::cout<< "Test 19 testing takeDamage() \n";
+    greg.setCurrentHealth(50);
+    greg.setCurrentArmour(50);
+    greg.takeDamage(50);
+    std::cout << greg.getCurrentArmour() << std::endl;
+    std::cout << greg.getCurrentHealth() << std::endl;
+    if (greg.getCurrentArmour() > 0)
+    {
+        return 1;
+    }
+    if (greg.getCurrentHealth() != 50)
+    {
+        return 1;
+    }
+
+    std::cout<< "Test 20 testing takeDamage() \n";
+    greg.takeDamage(10);
+    std::cout << greg.getCurrentArmour() << std::endl;
+    std::cout << greg.getCurrentHealth() << std::endl;
+    if (greg.getCurrentArmour() > 0)
+    {
+        return 1;
+    }
+    if (greg.getCurrentHealth() != 40)
+    {
+        return 1;
+    }
+
+
+    std::cout<< "Test 21 testing takeDamage() \n";
+    greg.setCurrentHealth(40);
+    greg.takeDamage(40);
+    std::cout << greg.getCurrentArmour() << std::endl;
+    std::cout << greg.getCurrentHealth() << std::endl;
+    if (greg.getCurrentArmour() > 0)
+    {
+        return 1;
+    }
+    if (greg.getCurrentHealth() > 0)
+    {
+        return 1;
+    }
+
+
+    std::cout<< "Test 22 testing setters() \n";
+    greg.setCurrentArmour(50);
+    greg.setCurrentHealth(50);
+    greg.setCurrentDamage(50);
+    std::cout << greg.getCurrentArmour() << std::endl;
+    std::cout << greg.getCurrentHealth() << std::endl;
+    std::cout << greg.getCurrentDamage() << std::endl;
+    if (greg.getCurrentArmour() != 50)
+    {
+        return 1;
+    }
+    if (greg.getCurrentHealth() != 50)
+    {
+        return 1;
+    }
+     if (greg.getCurrentDamage() != 50)
+    {
+        return 1;
+    }
+
+    std::cout<< "Test 23 testing fight, boss dies \n";
+    greg.fight(greg, steve);
+    std::cout<< "done \n";
+
+
+    std::cout<< "Test 24 testing receive attack \n";
+
+    Player Phil("Phil", 10, 15, 20, 0);
+    Phil.receiveAttack(10);
+    Phil.getCurrentArmour();
+    if (Phil.getCurrentArmour() != 10)
+    {
+        return 1;
+    }
+
+     std::cout<< "Test 25 testing print inventory() \n";
+     Phil.printInventory();
+
+     std::cout<< "Test 26 testing equip item() and getinventoryslot() \n";
+     Item ChestPlate("plate", 0, 0, 4, 0);
+     Phil.equipItem(&ChestPlate);
+     Phil.getInventorySlot(0);
+     
+
 }
